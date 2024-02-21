3be5a550c4a910d4e2403dd7a0a99edce0249851
IsaRakhatbekov <isarahatbekov250@gmail.com>
2024-02-21 15:45:06 +0600

    new video section

--- src/pages/Videos/Videos.tsx
+++ src/pages/Videos/Videos.tsx
@@ -3,79 +3,194 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './videos.scss';
 
-interface AccordionProps {
-  index: number;
-  title: string;
-  videoId: string;
-  openIndex: number;
-  setOpenIndex: React.Dispatch<React.SetStateAction<number>>;
-}
+// interface AccordionProps {
+//   index: number;
+//   title: string;
+//   videoId: string;
+//   openIndex: number;
+//   setOpenIndex: React.Dispatch<React.SetStateAction<number>>;
+// }
 
-const Accordion: React.FC<AccordionProps> = ({ index, title, videoId, openIndex, setOpenIndex }) => {
-  const isOpen = index === openIndex;
-  const contentRef = useRef<HTMLDivElement>(null);
+// const Accordion: React.FC<AccordionProps> = ({ index, title, videoId, openIndex, setOpenIndex }) => {
+//   const isOpen = index === openIndex;
+//   const contentRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (contentRef.current) {
-      contentRef.current.style.maxHeight = isOpen
-        ? `${contentRef.current.scrollHeight}px`
-        : '0';
-    }
-  }, [isOpen]);
+//   useEffect(() => {
+//     if (contentRef.current) {
+//       contentRef.current.style.maxHeight = isOpen
+//         ? `${contentRef.current.scrollHeight}px`
+//         : '0';
+//     }
+//   }, [isOpen]);
 
-  const handleToggle = () => {
-    setOpenIndex(isOpen ? -1 : index);
-  };
+//   const handleToggle = () => {
+//     setOpenIndex(isOpen ? -1 : index);
+//   };
 
-  return (
-    <div className={`accordion ${isOpen ? 'open' : ''}`}>
-      <div className="accordion__header" onClick={handleToggle}>
-        <h3>{title}</h3>
-      </div>
-      <div
-        ref={contentRef}
-        className={`accordion__content ${isOpen ? 'open' : ''}`}
-      >
-        {isOpen && (
-          <div className="accordion__iframe-wrapper">
-            <iframe
-              src={`https://www.youtube.com/embed/${videoId}?autoplay=1&muted=1`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-            {/* <iframe
-              src={`https://www.youtube.com/embed/${videoId}`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe> */}
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className={`accordion ${isOpen ? 'open' : ''}`}>
+//       <div className="accordion__header" onClick={handleToggle}>
+//         <h3>{title}</h3>
+//       </div>
+//       <div
+//         ref={contentRef}
+//         className={`accordion__content ${isOpen ? 'open' : ''}`}
+//       >
+//         {isOpen && (
+//           <div className="accordion__iframe-wrapper">
+//             <iframe
+//               src={`https://www.youtube.com/embed/${videoId}?autoplay=1&muted=1`}
+//               title="YouTube video player"
+//               frameBorder="0"
+//               allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//               allowFullScreen
+//             ></iframe>
+//             {/* <iframe
+//               src={`https://www.youtube.com/embed/${videoId}`}
+//               title="YouTube video player"
+//               frameBorder="0"
+//               allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//               allowFullScreen
+//             ></iframe> */}
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
 const Videos: React.FC = () => {
   const [openIndex, setOpenIndex] = useState<number>(-1);
 
   return (
-    <div className="container">
-      <div className="app">
-        <Accordion index={0} title="приветствие" videoId="M3AGJUwBH0k" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={1} title="Почему стоит выбрать MetaLabs" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={3} title="Из-за чего выбирают IT" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={4} title="Какие направления в IT актуальны" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={5} title="Сложно ли учиться на программиста" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={6} title="Убедись в качестве преподавания на 3 минуты" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={7} title="Почему стоит доверять нашим менторам" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={8} title="Чем наше обучение отличается от вузов" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={9} title="MetaLabs - это еще и IT компания" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={10} title="Как MetaLabs помогает в трудоустройстве и развитии карьеры" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
-        <Accordion index={11} title="У вас остались вопросы?" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    // <div className="container">
+    //   <div className="app">
+    //     <Accordion index={0} title="приветствие" videoId="M3AGJUwBH0k" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={1} title="Почему стоит выбрать MetaLabs" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={3} title="Из-за чего выбирают IT" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={4} title="Какие направления в IT актуальны" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={5} title="Сложно ли учиться на программиста" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={6} title="Убедись в качестве преподавания на 3 минуты" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={7} title="Почему стоит доверять нашим менторам" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={8} title="Чем наше обучение отличается от вузов" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={9} title="MetaLabs - это еще и IT компания" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={10} title="Как MetaLabs помогает в трудоустройстве и развитии карьеры" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //     <Accordion index={11} title="У вас остались вопросы?" videoId="u31qwQUeGuM" openIndex={openIndex} setOpenIndex={setOpenIndex} />
+    //   </div>
+    // </div>
+    <div className="video">
+      <div className="video__container container">
+        <div className="video__top video-top">
+          <h2 className="video-top__title">
+            Title
+          </h2>
+          <p className="video-top__text">
+            Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle
+          </p>
+          <div className="video-top__wrapper">
+            <div className="video-top__video">
+            {/* THIS PLACE FOR VIDEO */}
+            </div>      
+            <div className="video-top__inner">
+              <p className="video-top__desc">
+                Знакомство с языком программирования - <a href="#">C#</a>
+              </p>
+              <a className="video-top__link" href="#">Подробнее</a>
+            </div>
+          </div>
+          <button className="video-top__btn">Оставить заявку</button>
+        </div>
+        <ul className="video__list">
+            <li className="video__card">
+              <div className="video__wrapper">
+                {/* here will be your teg for video */}
+              </div>
+              <div className="video__desc-wrapper">
+                <p className="video__desc">
+                  Знакомство с языком программирования - <a href="#">C#</a>
+                </p>
+                <a className="video__link" href="#">Подробнее</a>
+              </div>
+            </li>
+                    <li className="video__card">
+              <div className="video__wrapper">
+                {/* here will be your teg for video */}
+              </div>
+              <div className="video__desc-wrapper">
+                <p className="video__desc">
+                  Знакомство с языком программирования - <a href="#">C#</a>
+                </p>
+                <a className="video__link" href="#">Подробнее</a>
+              </div>
+            </li>
+                    <li className="video__card">
+              <div className="video__wrapper">
+                {/* here will be your teg for video */}
+              </div>
+              <div className="video__desc-wrapper">
+                <p className="video__desc">
+                  Знакомство с языком программирования - <a href="#">C#</a>
+                </p>
+                <a className="video__link" href="#">Подробнее</a>
+              </div>
+            </li>
+                    <li className="video__card">
+              <div className="video__wrapper">
+                {/* here will be your teg for video */}
+              </div>
+              <div className="video__desc-wrapper">
+                <p className="video__desc">
+                  Знакомство с языком программирования - <a href="#">C#</a>
+                </p>
+                <a className="video__link" href="#">Подробнее</a>
+              </div>
+            </li>
+                    <li className="video__card">
+              <div className="video__wrapper">
+                {/* here will be your teg for video */}
+              </div>
+              <div className="video__desc-wrapper">
+                <p className="video__desc">
+                  Знакомство с языком программирования - <a href="#">C#</a>
+                </p>
+                <a className="video__link" href="#">Подробнее</a>
+              </div>
+            </li>
+                    <li className="video__card">
+              <div className="video__wrapper">
+                {/* here will be your teg for video */}
+              </div>
+              <div className="video__desc-wrapper">
+                <p className="video__desc">
+                  Знакомство с языком программирования - <a href="#">C#</a>
+                </p>
+                <a className="video__link" href="#">Подробнее</a>
+              </div>
+            </li>
+                    <li className="video__card">
+              <div className="video__wrapper">
+                {/* here will be your teg for video */}
+              </div>
+              <div className="video__desc-wrapper">
+                <p className="video__desc">
+                  Знакомство с языком программирования - <a href="#">C#</a>
+                </p>
+                <a className="video__link" href="#">Подробнее</a>
+              </div>
+            </li>
+                    <li className="video__card">
+              <div className="video__wrapper">
+                {/* here will be your teg for video */}
+              </div>
+              <div className="video__desc-wrapper">
+                <p className="video__desc">
+                  Знакомство с языком программирования - <a href="#">C#</a>
+                </p>
+                <a className="video__link" href="#">Подробнее</a>
+              </div>
+            </li>
+
+        </ul>
       </div>
     </div>
   );
